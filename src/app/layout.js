3ac5e354d9b5d0b401553e43e74c2d9536058e17
Leon Gabriel <g73346920@gmail.com>
2024-10-10 00:51:17 +0300

    Chored:Favicon added

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.jpg"/>
       </head>
       <body>
         <DataProvider>
