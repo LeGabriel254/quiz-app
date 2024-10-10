7c90d129fac27c66310dcf6d231f5303551e640b
Leon Gabriel <g73346920@gmail.com>
2024-10-10 09:19:42 +0300

    Function to check if the user has answered all question in the quiz added

--- src/app/components/quizContent.js
+++ src/app/components/quizContent.js
@@ -18,15 +18,24 @@ export default function QuizContent(){
     useEffect(()=>{
         // Define a mapping between subjects and corresponding background CSS classes
         const bg_classes = {"HTML":"bg_html", "CSS":"bg_css", "JavaScript":"bg_js", "Accessibility":"bg_access"};
+
+        
+        // Check if quiz data and subject parameter from the URL are available
         if(data_result.data && searchParams.get('subject')){
+              // Filter the data to find the quiz for the selected subject
             const filtered = data_result.data.filter((item)=>item.title === searchParams.get('subject')); 
+             
+            // Update the result state with the filtered quiz data
             setResult(filtered[0] || null);
+        
             data_result.setTopicLogo(filtered[0].icon);
             data_result.setTopicTitle(filtered[0].title);
             data_result.setTopicBgClass(bg_classes[filtered[0].title]);  
         }
     },[data_result, searchParams, result])
+      // Check if the user has answered all questions in the quiz
     if(result && data_result.counter > result.questions.length){
+    // If the quiz is completed, show the quiz completion screen and the user's score
         return(
             <>
             <section>
@@ -38,6 +47,7 @@ export default function QuizContent(){
             </>
         )
     }
+     // If the quiz is not completed, show the question and answer sections
     return(
         <>
             <section>
