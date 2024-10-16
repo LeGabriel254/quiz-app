1a4c9479e657aed4b79104843817518070350d24
Leon Gabriel <g73346920@gmail.com>
2024-10-17 00:08:23 +0300

    chored:export Title

--- src/app/components/title.js
+++ src/app/components/title.js
@@ -1,6 +1,6 @@
 'use client'
-export default function Title({title_part1, title_part2}){
-    return(
+export default function Title({ title_part1, title_part2 }) {
+    return (
         <>
             <h2>{title_part1}</h2>
             <strong>{title_part2}</strong>
