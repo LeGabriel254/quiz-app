d7be822a9e97a717d3871fe364bb54a4e39cf256
Leon Gabriel <g73346920@gmail.com>
2024-10-17 00:18:02 +0300

    ternary operator used

--- src/app/components/subjectTitle.js
+++ src/app/components/subjectTitle.js
@@ -3,25 +3,25 @@ import Image from 'next/image';
 import { useSearchParams } from 'next/navigation';
 import { useEffect } from 'react';
 
-export default function SubjectTitle({icon_url, title, bg_class }){
+export default function SubjectTitle({ icon_url, title, bg_class }) {
     const searchParams = useSearchParams();
 
-    useEffect(()=>{
-        if(searchParams.get('subject')){
-            document.querySelectorAll(".subject_title_container").forEach((item)=>{
+    useEffect(() => {
+        if (searchParams.get('subject')) {
+            document.querySelectorAll(".subject_title_container").forEach((item) => {
                 item.classList.add("visible");
             })
-            
-        }else{
-            document.querySelectorAll(".subject_title_container").forEach((item)=>{
+
+        } else {
+            document.querySelectorAll(".subject_title_container").forEach((item) => {
                 item.classList.remove("visible");
             })
         }
-    },[searchParams])
+    }, [searchParams])
 
     return (
         <div className='subject_title_container'>
-            <Image src={icon_url ? icon_url : '/images/icon-html.svg'} className= {`header_subject_icon ${ bg_class ? bg_class : ''}`} alt="topic icon" width={40} height={40}/>
+            <Image src={icon_url ? icon_url : '/images/icon-html.svg'} className={`header_subject_icon ${bg_class ? bg_class : ''}`} alt="topic icon" width={40} height={40} />
             <h1 className='header_subject_title'>{title ? title : 'Subject'}</h1>
         </div>
     )
