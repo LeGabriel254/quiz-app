e9bf1a2e5fed091a18890811d71fe692f73c1794
Leon Gabriel <g73346920@gmail.com>
2024-10-17 00:12:18 +0300

    loading fallback using the suspense

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -5,22 +5,22 @@ import ThemeSwitcher from "./themeSwitcher";
 import SubjectTitle from './subjectTitle';
 import { Suspense } from 'react';
 
-export default function Header(){
+export default function Header() {
   const data_result = useData();
   //to check whether data is loaded
-if(data_result){
-  return (
+  if (data_result) {
+    return (
       <header>
-        <Suspense fallback={ <h3>Loading ...</h3>}>
-          <SubjectTitle icon_url={data_result.topicLogo} title={data_result.topicTitle} bg_class={data_result.topicBgClass}/>
+        <Suspense fallback={<h3>Loading ...</h3>}>
+          <SubjectTitle icon_url={data_result.topicLogo} title={data_result.topicTitle} bg_class={data_result.topicBgClass} />
         </Suspense>
-        <ThemeSwitcher/>
+        <ThemeSwitcher />
       </header>
-  )
-}else{
+    )
+  } else {
 
-  return(
-    <h3>Loading ...</h3> //if the data hasn't load yet
-  )
-}
+    return (
+      <h3>Loading ...</h3> //if the data hasn't load yet
+    )
+  }
 }
