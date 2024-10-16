f7f10bb7115b549a05815bc3de4cff2e1b530dfa
Leon Gabriel <g73346920@gmail.com>
2024-10-17 00:18:50 +0300

    Initial commit

--- src/app/questions/page.js
+++ src/app/questions/page.js
@@ -1,10 +1,10 @@
 import { Suspense } from "react";
 import QuizContent from "../components/quizContent";
 
-export default function Quiz(){
-    return(
+export default function Quiz() {
+    return (
         <Suspense fallback={<h3>Loading quiz...</h3>}>
-            <QuizContent/>
+            <QuizContent />
         </Suspense>
     )
 }
