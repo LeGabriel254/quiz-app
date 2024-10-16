6871cb6e25a2d0b6d053097d1a68d1adc9a004fb
Leon Gabriel <g73346920@gmail.com>
2024-10-17 00:13:02 +0300

    Modified

--- src/app/components/questionSection.js
+++ src/app/components/questionSection.js
@@ -1,25 +1,25 @@
 'use client'
 
-import { useEffect} from 'react';
+import { useEffect } from 'react';
 
-export default function QuestionSection({data, data_result}){
-    useEffect(()=>{
-        if(data && data_result){
-            document.documentElement.style.setProperty('--range_status', `${data_result ? data_result.counter*10 : 0}%`);
+export default function QuestionSection({ data, data_result }) {
+    useEffect(() => {
+        if (data && data_result) {
+            document.documentElement.style.setProperty('--range_status', `${data_result ? data_result.counter * 10 : 0}%`);
         }
-    },[data,data_result])
+    }, [data, data_result])
 
-    if(data && data_result){
-        return(
+    if (data && data_result) {
+        return (
             <>
                 <p>Question {data_result.counter} of {data.questions.length}</p>
-                <h3>{data.questions[data_result.counter-1].question}</h3>
+                <h3>{data.questions[data_result.counter - 1].question}</h3>
                 <label htmlFor='progress' className='progress_label'>progress</label>
-                <input id='progress' tabIndex="-1" type="range" className='range_status' min={1} max={10} value={data_result.counter} readOnly/>
+                <input id='progress' tabIndex="-1" type="range" className='range_status' min={1} max={10} value={data_result.counter} readOnly />
             </>
         )
-    }else{
-        return(
+    } else {
+        return (
             <h3>Loading question ...</h3>
         )
     }
