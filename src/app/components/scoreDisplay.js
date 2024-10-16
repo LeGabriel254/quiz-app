77cd0c6d7a79de8548d22d4d1a24e52a750c356c
Leon Gabriel <g73346920@gmail.com>
2024-10-17 00:14:51 +0300

    Modified

--- src/app/components/scoreDisplay.js
+++ src/app/components/scoreDisplay.js
@@ -3,17 +3,17 @@ import { Suspense } from "react";
 import SubjectTitle from "./subjectTitle";
 import { useRouter } from 'next/navigation'
 
-export default function ScoreDisplay({data_result}){
+export default function ScoreDisplay({ data_result }) {
     const router = useRouter()
-    const playAgain = () =>{
+    const playAgain = () => {
         router.push('/');
     }
 
-    return(
+    return (
         <>
             <div className="score_container">
-                <Suspense fallback= {<Loading/>}>
-                    <SubjectTitle icon_url={data_result.topicLogo} title={data_result.topicTitle} bg_class={data_result.topicBgClass}/>  
+                <Suspense fallback={<Loading />}>
+                    <SubjectTitle icon_url={data_result.topicLogo} title={data_result.topicTitle} bg_class={data_result.topicBgClass} />
                 </Suspense>
                 <h4 className="score">{data_result.score ? data_result.score : 0}</h4>
                 <span className="score_msg">out of 10</span>
